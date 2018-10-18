0b2ffe3616d7531c1bd68fe6e50659458d0be38e
primerX <1034805730@qq.com>
2018-10-18 19:59:42 +0800

    2018.10.18

--- B1011.A+B和C.cpp
+++ B1011.A+B和C.cpp
@@ -1,1 +1,20 @@
+#include<cstdio>
 
+int main()
+{
+	int n;
+	long long A, B, C; 
+	scanf("%d", &n);
+	int cnt = 1;
+	for(int i=0; i<n; i++)
+	{
+		scanf("%lld %lld %lld", &A, &B, &C);
+		if(A + B > C){
+			printf("Case #%d: true\n", cnt);
+		}else{
+			printf("Case #%d: false\n", cnt);
+		}
+		cnt++;
+	}
+	return 0;
+ } 
