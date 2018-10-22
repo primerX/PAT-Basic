5e247d5bf8f38894acdb7b8c27caf073d7f89ed3
primerX <1034805730@qq.com>
2018-10-22 20:43:48 +0800

    2018.10.22
    初始hashTable[128] 不能为 true
    不然程序编译可以通过，但会在判断时卡主，将不会产生结果

---
+++ B1029.旧键盘.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+#include<string>
+#include<iostream>
+using namespace std;
+
+bool hashTable[1000] = {false};	//表示字符是否已经被输出 
+
+int main()
+{
+	string str1, str2;
+	getline(cin, str1);
+	getline(cin, str2);
+	int len1 = str1.length();
+	int len2 = str2.length();
+	for(int i = 0; i < len1; i++) {
+        int j;
+        char c1, c2;
+        for(j = 0; j < len2; j++) {
+            c1 = str1[i];
+            c2 = str2[j];
+            if(c1 >= 'a' && c1 <= 'z') c1 -= 32;
+            if(c2 >= 'a' && c2 <= 'z') c2 -= 32;
+            if(c1 == c2) break;
+        }
+    
+        if(j == len2 && hashTable[c1] == false) {
+            printf("%c", c1);
+            hashTable[c1] = true;
+        }
+    }
+	return 0;
+}
