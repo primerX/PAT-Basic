e8199782e39f3ef68d4806ea352781c7371456b7
primerX <1034805730@qq.com>
2018-10-28 06:41:36 +0800

    2018.10.28
    尽可能的使用c++，摒弃以前用c的习惯

--- B1011.A+B和C.cpp
+++ B1011.A+B和C.cpp
@@ -1,20 +1,18 @@
-#include<cstdio>
+#include<iostream>
+using namespace std;
 
 int main()
 {
-	int n;
-	long long A, B, C; 
-	scanf("%d", &n);
-	int cnt = 1;
-	for(int i=0; i<n; i++)
-	{
-		scanf("%lld %lld %lld", &A, &B, &C);
-		if(A + B > C){
-			printf("Case #%d: true\n", cnt);
+	int T, cnt = 1;		//cnt 用来控制输出格式 
+	long long a, b, c;
+	cin >> T;
+	for(int i=0; i<T; i++){
+		cin >> a >> b >> c;
+		if(a + b > c){
+			printf("Case #%d: true\n", cnt++);
 		}else{
-			printf("Case #%d: false\n", cnt);
+			printf("Case #%d: false\n", cnt++);
 		}
-		cnt++;
 	}
 	return 0;
  } 
